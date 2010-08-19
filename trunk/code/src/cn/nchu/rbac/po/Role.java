323050bb17f8167a9cebb28fdbf35fe5e501ec57
frueyang <frueyang@46fc12a7-2450-d479-d9a0-ce42ef7b52c6>
2010-08-19 12:38:59 +0000

    新建工程目录结构，写好了一部分 service 接口

---
+++ trunk/code/src/cn/nchu/rbac/po/Role.java
@@ -0,0 +1,5 @@
+package cn.nchu.rbac.po;
+
+public class Role {
+
+}
---
+++ trunk/code/src/cn/nchu/rbac/util/WebException.java
@@ -0,0 +1,15 @@
+package cn.nchu.rbac.util;
+
+/**
+ * 自定义异常对象
+ * @author Administrator
+ *
+ */
+public class WebException extends Throwable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
